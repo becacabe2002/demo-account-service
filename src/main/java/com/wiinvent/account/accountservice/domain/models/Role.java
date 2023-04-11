d5b5e88c72f07632f657649e04b596812ea120f1
Minh Tu Ngo <tungominh.hust@gmail.com>
2023-04-11 09:24:02 +0700

    add models

---
+++ src/main/java/com/wiinvent/account/accountservice/domain/models/Role.java
@@ -0,0 +1,7 @@
+package com.wiinvent.account.accountservice.domain.models;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/wiinvent/account/accountservice/domain/models/UserRepository.java
@@ -0,0 +1,14 @@
+package com.wiinvent.account.accountservice.domain.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * Define methods for working with user data in DB
+ * Provide an abstraction layer between application and DB,
+ * so that we can perform CRUD operations without having to write SQL queries
+ */
+public interface UserRepository extends JpaRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
+}
